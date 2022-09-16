4228a8a9aee468e4b551a36d1a22998845c00e79
Luciekinyanjui <lucy.wambui@moringaschool.com>
2022-09-16 23:32:38 +0300

    Lab complete

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
 class Student 
-
+    class Student < User
+        def initialize
+            @knowledge = []
+        end
+    
+        def learn(student_input)
+            @knowledge << student_input
+        end
+    
+        def knowledge
+            @knowledge
+        end
 end
